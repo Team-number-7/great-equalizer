a7b51a9c4abd95860c2786bf1a1736ef66d20373
Blad <valtfras@gmail.com>
2022-03-12 17:05:51 +0300

    eslint, feature toggle script, modified circleci config file

---
+++ index.ts
@@ -0,0 +1,7 @@
+const isUserName = true;
+window.onload = function featureToggle() {
+  if (!isUserName) {
+    const logo = document.getElementById('header__logo');
+    logo?.setAttribute('style', 'visibility: hidden;');
+  }
+};
