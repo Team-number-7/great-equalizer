e651602ec5914d1009ab5d30edee35d6d395562d
Vonamugan <Vonamugan@users.noreply.github.com>
2022-04-17 13:58:03 +0000

    Built artifacts of 63851c5 [ci skip]

--- index.js
+++
@@ -1,8 +0,0 @@
-const isUserName = true;
-window.onload = function featureToggle() {
-    if (!isUserName) {
-        const logo = document.getElementById('header__logo');
-        logo === null || logo === void 0 ? void 0 : logo.setAttribute('style', 'visibility: hidden;');
-    }
-};
-//# sourceMappingURL=index.js.map
