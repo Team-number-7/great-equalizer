70e83ea7f58763e9d5d5cbae0729e551b5f1a9d4
Andrey <41819110+Vonamugan@users.noreply.github.com>
2022-04-24 18:10:07 +0300

    refactor test

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,5 +1,15 @@
 import { sum } from './sum';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+    // Arrange
+    const expected_first_arg = 1;
+    const expected_second_arg = 2;
+    const expected_result = 3;
+    
+    // Act
+    const actual_result = sum(expected_first_arg, expected_second_arg);
+    
+    // Assert
+    expect(actual_result).toBe(expected_result)
 });
+
